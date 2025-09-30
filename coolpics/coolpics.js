cf543050c3c3d3c542c27c4f1d938c129e6fc010
ajbradshaw25 <Sapphireflowers30@gmail.com>
2025-09-30 15:22:12 -0700

    wdd

--- coolpics/coolpics.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const menuButton = document.getElementById("menuButton");
-  const navLinks = document.getElementById("navLinks");
-
-  menuButton.addEventListener("click", function() {
-    navLinks.classList.toggle("open");
-  });
-});
