5956384c504a92807bac28dcc31b846060435d85
ajbradshaw25 <Sapphireflowers30@gmail.com>
2025-10-21 14:40:00 -0700

    wdd

--- arrays.js
+++ arrays.js
@@ -1,34 +1,22 @@
-let new_array = arr.map(function callback( currentValue[, index[, array]]) {
-// return element for new_array
-}[, thisArg])
+let new_array = Array.map(function callback( currentValue[, index[, array]]) {
+    // return element for new array
+})[, thisArg]
 
+// example 1
 const steps = ["one", "two", "three"];
-function listTemplate(step) {
-  return `<li>${step}</li>`;
+const listTemplate(step) {
+    return `<li>${step}</li>`;
 }
 const stepsHtml = steps.map(listTemplate);
-document.querySelector("#myList").innerHTML = stepsHtml.join()
-
+document.querySelector("#myList").innerHTML = stepsHtml.join();
 
 // example 2
-const grades = ["A", "B", "A"];
-function convertGradeToPoints(grade) {
-  let points = 0;
-  if (grade === "A") {
-    points = 4;
-  } else if (grade === "B") {
-    points = 3;
-  }
-  return points;
-}
-const gpaPoints = grades.map(convertGradeToPoints);
-const pointsTotal = gpaPoints.reduce((total, item) => total + item);
-const gpa = gpaPoints.reduce((total, item) => total + item) / gpaPoints.length;
-
-const words = ["watermelon", "peach", "apple", "tomato", "grape"];
-const shortWords = words.filter((word) => word.length < 6);
-
-// improved luckyNumber
+// is the luckyNumber in the list?
 const myArray = [12, 34, 21, 54];
 const luckyNumber = 21;
-let luckyIndex = myArray.indexOf(luckyNumber);
+let luckyIndex = -1;
+myArray.forEach(function (item, index) {
+  if (item === luckyNumber) {
+    luckyIndex = index;
+  }
+});
