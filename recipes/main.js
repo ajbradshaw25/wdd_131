e7467a2e6002e11e92058082f48fb9e673067203
ajbradshaw25 <Sapphireflowers30@gmail.com>
2025-11-13 12:08:49 -0700

    wdd

---
+++ recipes/main.js
@@ -0,0 +1,6 @@
+import { RECIPES } from "./recipes.mjs";
+
+console.log("Recipes loaded:", RECIPES.length);
+
+// TODO: Write code here to dynamically render the recipe cards 
+// based on the data in recipes.mjs.
