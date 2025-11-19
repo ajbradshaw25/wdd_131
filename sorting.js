d5284bab2b7cd56e0e9a678d040c619294311acc
ajbradshaw25 <Sapphireflowers30@gmail.com>
2025-11-18 17:10:15 -0700

    wdd

--- sorting.js
+++ sorting.js
@@ -67,6 +67,16 @@ const hikes = [
 ];
  const simpleList = ["oranges", "grapes", "lemons", "apples", "Bananas", "watermelons", "coconuts", "broccoli", "mango"];
 
+ function compareFn(a,b) {
+      if (a > b) {
+        return -1;
+      } else if (a < b) {
+        return 1;
+      }
+     // a must be equal to b
+     return 0;
+    }
+    const anotherSort = simpleList.sort(compareFn)
 function searchList(list, query) {
     function searchCallback(string) {
         return string.toLowerCase().includes(query.toLowerCase());
